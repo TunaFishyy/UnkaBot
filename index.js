572baad61cd3d48a6c6220d40ad8381425f0ebd2
TunaFishyy <lantunovic93@gmail.com>
2022-01-25 21:39:26 +0100

    jog

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
     console.log('Bot se ukljucio!')
 })
 
-const images = ['https://i.imgur.com/He1yFhG.jpeg', 'https://imgur.com/gallery/O0XD03F', 'https://i.imgur.com/chiIGfLb.jpg', 'https://i.imgur.com/nP45XGvb.jpg',
+const images = ['https://i.imgur.com/He1yFhG.jpeg', 'https://i.imgur.com/QMJuIcDb.jpg', 'https://i.imgur.com/chiIGfLb.jpg', 'https://i.imgur.com/nP45XGvb.jpg',
                 'https://i.imgur.com/TkzGmjJb.jpg', 'https://i.imgur.com/uqJ2ci5b.jpg', 'https://i.imgur.com/fK7NYeNb.jpg', 'https://i.imgur.com/HsPuvfvb.jpg'];
 
 var prefix = '.'
@@ -22,6 +22,7 @@ const embed = new MessageEmbed()
     .setColor('AQUA')
     .setTitle('Keva ti debela')
     .setImage(images[Math.floor(Math.random()*images.length)]);
+
 client.on('messageCreate', (msg) => {
     if (msg.author.bot) return;
 
