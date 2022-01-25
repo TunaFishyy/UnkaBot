a090a679964c525fdd8578bb0f981cce427a941e
TunaFishyy <lantunovic93@gmail.com>
2022-01-25 19:00:59 +0100

    fdafadf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import DiscordJS, { Intents, Message } from 'discord.js'
+import DiscordJS, { Intents, Message, MessageEmbed } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -13,13 +13,24 @@ client.on('ready', () => {
     console.log('Bot se ukljucio!')
 })
 
+const images = ['https://i.imgur.com/He1yFhG.jpeg', 'https://imgur.com/gallery/O0XD03F', 'https://i.imgur.com/chiIGfLb.jpg', 'https://i.imgur.com/nP45XGvb.jpg',
+                'https://i.imgur.com/TkzGmjJb.jpg', 'https://i.imgur.com/uqJ2ci5b.jpg', 'https://i.imgur.com/fK7NYeNb.jpg', 'https://i.imgur.com/HsPuvfvb.jpg'];
+const embed = new MessageEmbed()
+    .setColor('AQUA')
+    .setTitle('Keva ti debela')
+    .setDescription('Rip BOZO L')
+    .setImage(images[Math.floor(Math.random()*images.length)]);
+
 var prefix = '.'
 
 client.on('messageCreate', (msg) => {
     if (msg.author.bot) return;
 
     if (msg.content.toLowerCase() === prefix+'mrs') {
-        msg.channel.send('Ma mrs ti')
+        msg.reply('Ma mrs ti')
+    }
+    if (msg.content.toLowerCase() === prefix+'debelsi') {
+        msg.reply({ embeds: [embed] });
     }
 })
 
