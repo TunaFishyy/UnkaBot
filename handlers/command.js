d4fa31a20b4ff7f9b59dc3cab97c42f5ba207d57
TunaFishyy <lantunovic93@gmail.com>
2022-01-27 16:45:33 +0100

    bra

--- handlers/command.js
+++ handlers/command.js
@@ -4,8 +4,8 @@ let table = new ascii("Commands");
 table.setHeading('Command', 'Load Status');
 
 module.exports = (client) => {
-    readdirSync('../commands').forEach(dir => {
-        const commands = readdirSync(`../commands/${dir}/`).filter(file => file.endsWith('.js'));
+    readdirSync("./commands").forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
             if (pull.name) {
