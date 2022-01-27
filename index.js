de536bb8ff3fd5516449075592bdf30d97404e99
TunaFishyy <lantunovic93@gmail.com>
2022-01-27 17:43:13 +0100

    update

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 require('dotenv').config();
-const { Client, Intents, Collection } = require('discord.js');
-const config = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES
+  ]
+});
 const fs = require('fs');
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-});
-client.commands = new Collection();
-client.aliases = new Collection();
-client.categories = new Collection();
+const { type } = require('os');
 
-["command", "events"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
 
-client.once('ready', () => {
-  console.log('Bot is on.');
-})
+  client.commands.set(command.name, command);
+}
+
+const arrayOfStatuses = ['M4LC0LM je gay', 'Dudlica ga dudla', 'PRO TIP: Ne igrajte wingman s infitorom', 'Jel vama keza krindz', 'Ico kid 011', 'Marusa gleda winxice',
+                         'Krtina'];
+
+client.on('ready', () => {
+  console.log(`Bot is online!`);
+  client.user.setPresence({
+    activities: [{
+      name: arrayOfStatuses[Math.floor(Math.random()*arrayOfStatuses.length)]
+    }]
+  });
+});
 
 client.on('messageCreate', async message => {
-  if (!message.content.startsWith(config.prefix) || !message.guild || message.author.bot) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd.length == 0) return;
-  let command = client.commands.get(cmd);
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-  if(command) command.run(client.message.args);
+  // Define Command Components //
+  const args = message.content.slice(config.pre).split(/ +/);
+  const command = args.shift().toLowerCase()
+
+  if (command == 'help') {
+      client.commands.get('help').execute(message)
+  }
 })
 
 client.login(process.env.TOKEN);
