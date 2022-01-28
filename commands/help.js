fa84bfbd17a18c1d065fff7e9dbecd2820d131f2
TunaFishyy <lantunovic93@gmail.com>
2022-01-28 08:54:17 +0100

    xd

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(message) {
         const embed = new MessageEmbed()
             .setTitle('Help Meni')
-            .setDescription('Prefix: **.**\nKomande: **mrs**, **debelsi**')
+            .setDescription('Prefix: **.**\nKomande: **mrs**, **debelsi**, **ping**')
             .setColor('AQUA');
  
         message.reply({embeds: [embed], allowedMentions: {repliedUser: false}})
