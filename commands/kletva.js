4be35c25f3eaeedfcbe4398bea30880c921f6d5a
TunaFishyy <lantunovic93@gmail.com>
2022-01-30 19:33:56 +0100

    s

--- commands/kletva.js
+++ commands/kletva.js
@@ -1,12 +1,11 @@
 const { MessageEmbed } = require('discord.js');
 
-var kletva1 = `Quo facto,\nfacio actum quod debet et maxime\n vult mittere omnia mala ad victimam <@${target.user.id}>`;
-
 module.exports = {
     name: 'kletva',
     description: 'Zaklij nekog.',
     execute(message) {
         const target = message.mentions.user.first();
+        var kletva1 = `Quo facto,\nfacio actum quod debet et maxime\n vult mittere omnia mala ad victimam <@${target.user.id}>`;
         if (!message.content.toLowerCase().includes(target)) {
             message.reply('Moras navesti na koga ces baciti kletvu. **.kletva @user**');
         } else {
@@ -14,8 +13,9 @@ module.exports = {
                 .setColor('DARK_BUT_NOT_BLACK')
                 .setTitle(`Bacam kletvu na <@${target.user.id}>!`)
                 .setDescription(kletva1)
-                .setImage(target.author.avatarURL)
-                .setImage();
+                .setImage(target.author.avatarURL);
+
+                message.reply({ embeds: [embed], allowedMentions: {repliedUser: false} });
         }
 
     }
