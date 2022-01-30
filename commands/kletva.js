689d2e78ec58e5860e3d602b5c7501a9e2b52a65
TunaFishyy <lantunovic93@gmail.com>
2022-01-30 19:42:51 +0100

    fff

--- commands/kletva.js
+++ commands/kletva.js
@@ -1,22 +1,21 @@
 const { MessageEmbed } = require('discord.js');
 
+
 module.exports = {
     name: 'kletva',
     description: 'Zaklij nekog.',
     execute(message) {
-        const target = message.mentions.user.first();
-        var kletva1 = `Quo facto,\nfacio actum quod debet et maxime\n vult mittere omnia mala ad victimam <@${target.user.id}>`;
-        if (!message.content.toLowerCase().includes(target)) {
-            message.reply('Moras navesti na koga ces baciti kletvu. **.kletva @user**');
-        } else {
-            const embed = new MessageEmbed()
-                .setColor('DARK_BUT_NOT_BLACK')
-                .setTitle(`Bacam kletvu na <@${target.user.id}>!`)
-                .setDescription(kletva1)
-                .setImage(target.author.avatarURL);
+        const Target = message.mentions.users.first() || message.author;
+        const Member = message.guild.members.cache.get(Target.id);
 
-                message.reply({ embeds: [embed], allowedMentions: {repliedUser: false} });
-        }
+        var kletva1 = `Quo facto,\nfacio actum quod debet et maxime\n vult mittere omnia mala ad victimam <@${Target.id}>`;
 
+        const Response = new MessageEmbed()
+            .setAuthor(`${Target.username}`, Target.displayAvatarURL({ dynamic: true }))
+            .setThumbnail(Target.displayAvatarURL({ dynamic: true }))
+            .setColor('DARK_BUT_NOT_BLACK')
+            .setDescription(kletva1);
+
+            message.reply({ embeds: [Response], allowedMentions: {repliedUser: false} });
     }
 }
