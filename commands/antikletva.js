10f755c3dac976798162a92501ff29ea61fffebc
TunaFishyy <lantunovic93@gmail.com>
2022-01-30 21:28:12 +0100

    wata

--- commands/antikletva.js
+++ commands/antikletva.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'AntiKletva',
-    description: 'CounterAttack .kletva',
+    description: 'CounterAttack kletva',
     execute(message) {
         try {
 
